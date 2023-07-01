f24f26dce036ce5a7c333be7dad1c1796609513a
James Boase <jamesboase123@gmail.com>
2023-07-01 13:06:35 -0400

    fix issue stopping eraser from being painted on

--- script.js
+++ script.js
@@ -5,81 +5,84 @@ let currentSize = 16;
 let rainbow = false;
 let eraser = false;
 
-function createGrid(size = 16){
+function createGrid(size = 16) {
     const hoveredSquares = document.querySelectorAll(".hovered");
     const divSquare = document.querySelector(".div-square");
     divSquare.innerHTML = "";
-for (let i = 0; i < size; i++){
-    const div = document.createElement("div");
-    div.classList.add("line");
-    divSquare.appendChild(div);
+    for (let i = 0; i < size; i++) {
+        const div = document.createElement("div");
+        div.classList.add("line");
+        divSquare.appendChild(div);
 
+    }
+
+    const squares = document.querySelectorAll(".line");
+    squares.forEach(div => {
+        for (let i = 0; i < size; i++) {
+            const newDiv = document.createElement("div")
+            newDiv.classList.add("div-child");
+            newDiv.setAttribute("style", `width: ${800 / size}px; height: ${800 / size}px;`);
+            newDiv.addEventListener("mouseover", addHoveredClass);
+            div.appendChild(newDiv);
+        }
+
+    });
 }
 
-const squares = document.querySelectorAll(".line");
-squares.forEach(div => {
-    for (let i = 0; i < size; i++){
-        const newDiv = document.createElement("div")
-        newDiv.classList.add("div-child");
-        newDiv.setAttribute("style", `width: ${800/size}px; height: ${800/size}px;`);
-        newDiv.addEventListener("mouseover", addHoveredClass);
-        div.appendChild(newDiv);
+function addHoveredClass(e) {
+    if (rainbow == true) {
+        this.style.backgroundColor = getRandomColor();
     }
-
-});
-}
-
-function addHoveredClass(e){
-    if (rainbow == true){
-    this.style.backgroundColor = getRandomColor();
+    else if (eraser == true) {
+        this.style.backgroundColor = "lightgrey";
     }
-    else if (eraser == true){
-    this.style.backgroundColor = "lightgrey";
-    }
-    else{
-    this.classList.add("hovered");
+    else {
+        this.style.backgroundColor = "black";
     }
 }
 createGrid();
 
-btn1.addEventListener("click", changeGridSize); 
+btn1.addEventListener("click", changeGridSize);
 btn2.addEventListener("click", rainbowColour);
 btn3.addEventListener("click", erase)
-function changeGridSize(e){
+function changeGridSize(e) {
     const newSize = parseInt(prompt("Enter a new grid size less than 100"));
-    if (typeof newSize == "string" || isNaN(newSize) || newSize < 1 || newSize > 100){
+    if (typeof newSize == "string" || isNaN(newSize) || newSize < 1 || newSize > 100) {
         alert("Invalid grid size!");
     }
     else {
-    currentSize = newSize;
-    createGrid(newSize);
+        currentSize = newSize;
+        createGrid(newSize);
     }
 }
 
-function rainbowColour(e){
-    if (rainbow == true){
+function rainbowColour(e) {
+    if (rainbow == true) {
         rainbow = false;
         btn2.style.backgroundColor = "white";
 
     }
     else {
-        eraser = false;
+        if (eraser == true) {
+            erase();
+        }
         rainbow = true;
         btn2.style.backgroundColor = "lightgreen";
- 
     }
 }
- 
-function erase(e){
-    if (eraser == true){
+
+function erase(e) {
+    if (eraser == true) {
         eraser = false;
         btn3.style.backgroundColor = "white";
     }
 
-    else{
-        rainbow = false;
+    else {
+        if (rainbow == true) {
+            rainbowColour();
+        }
         eraser = true;
-        btn3.style.backgroundColor = "lightgreen"; 
+        btn3.style.backgroundColor = "lightgreen";
     }
 }
 
@@ -87,8 +90,7 @@ function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
     for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+        color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
-  }
-  
+}
